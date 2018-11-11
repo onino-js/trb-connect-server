78478904498af7121948554afdb83d3520820733
onino <seba.pinard@gmail.com>
2018-11-11 17:45:31 +0100

    change env

--- ormconfig.js
+++ ormconfig.js
@@ -32,9 +32,9 @@ const config =
         },
       }
     : {
-        type: 'mysql',
+        type: 'postgres',
         host: process.env.DB_HOST,
-        port: 3306,
+        port: process.env.PORT,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
