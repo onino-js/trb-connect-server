584513ae8a2b656ae41dfc81c213ba63185ea906
onino <seba.pinard@gmail.com>
2018-11-12 19:57:41 +0100

    change orm_config

--- ormconfig.js
+++ ormconfig.js
@@ -33,12 +33,12 @@ const config =
       }
     : {
         type: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        // url: process.env.DATABASE_URL,
+        // host: process.env.DB_HOST,
+        // port: process.env.DB_PORT,
+        // username: process.env.DB_USER,
+        // password: process.env.DB_PASSWORD,
+        // database: process.env.DB_NAME,
+        url: process.env.DATABASE_URL,
         entities: [entitiesDir],
         migrations: [migrationsDir],
         synchronize: true,
