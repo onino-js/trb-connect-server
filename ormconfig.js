a97e1f113c5834d56d2e6c9c3ae0916ac059c028
onino <seba.pinard@gmail.com>
2018-11-12 18:41:56 +0100

    change seed script

--- ormconfig.js
+++ ormconfig.js
@@ -33,11 +33,12 @@ const config =
       }
     : {
         type: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        // host: process.env.DB_HOST,
+        // port: process.env.DB_PORT,
+        // username: process.env.DB_USER,
+        // password: process.env.DB_PASSWORD,
+        // database: process.env.DB_NAME,
+        url: process.env.DB_URL,
         entities: [entitiesDir],
         migrations: [migrationsDir],
         synchronize: true,
